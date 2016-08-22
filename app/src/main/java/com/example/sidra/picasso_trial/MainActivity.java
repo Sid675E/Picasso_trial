5dd50a5c44e589dc981abc9b592489bdfee92b7e
Sid796 <sidraeffendi@gmail.com>
2016-08-22 12:15:06 +0530

    one image url display working

--- app/src/main/java/com/example/sidra/picasso_trial/MainActivity.java
+++ app/src/main/java/com/example/sidra/picasso_trial/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.sidra.picasso_trial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declaring our ImageView
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initializing the ImageView
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+
+       Picasso.with(this)
+                .load("https://upload.wikimedia.org/wikipedia/commons/d/d5/Mona_Lisa_(copy,_Hermitage).jpg")
+                .into(imageView);
+
     }
 }
