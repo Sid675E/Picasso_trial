5ce05dc9bb67854da15cce261a4c4b9d0fe6cf18
Sid796 <sidraeffendi@gmail.com>
2016-08-23 14:27:41 +0530

    displays photo from S3 bucket

--- app/src/main/java/com/example/sidra/picasso_trial/MainActivity.java
+++ app/src/main/java/com/example/sidra/picasso_trial/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.sidra.picasso_trial;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
+import com.amazonaws.HttpMethod;
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.S3Object;
+import com.squareup.picasso.Picasso;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     AmazonS3 s3;
     TransferUtility transferUtility;
 
-
+    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +39,20 @@ public class MainActivity extends AppCompatActivity {
         //Initializing the ImageView
         imageView = (ImageView) findViewById(R.id.imageView);
 
+        credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),    /* get the context for the application */
+                "ap-northeast-1:2c9313f6-ef22-44e7-bdb3-2a41f5b155a3",    /* Identity Pool ID */
+                Regions.AP_NORTHEAST_1          /* Region for your identity pool--US_EAST_1 or EU_WEST_1*/
+        );
 
+        s3 = new AmazonS3Client(credentialsProvider);
+        s3.setRegion(Region.getRegion(Regions.AP_NORTHEAST_1));
+
+        transferUtility = new TransferUtility(s3, getApplicationContext());
+
+        //DownloadFilesTask downloadTask = new DownloadFilesTask();
+
+        setImage(getApplicationContext(),imageView,1);
 
 
 
@@ -42,64 +62,16 @@ public class MainActivity extends AppCompatActivity {
     public void onStart(){
         super.onStart();
 
-        DownloadFilesTask downloadTask = new DownloadFilesTask();
-        downloadTask.execute();
+
     }
 
 
-    private class DownloadFilesTask extends AsyncTask<Void, Void, Void> {
+    private void setImage(final Context context, final ImageView iv, final int id) {
 
+        new AsyncTask<Void, Void, Uri>() {
 
-        @Override
-        protected Void doInBackground(Void... params) {
-
-            credentialsProvider = new CognitoCachingCredentialsProvider(
-                    getApplicationContext(),    /* get the context for the application */
-                    "ap-northeast-1:2c9313f6-ef22-44e7-bdb3-2a41f5b155a3",    /* Identity Pool ID */
-                    Regions.AP_NORTHEAST_1          /* Region for your identity pool--US_EAST_1 or EU_WEST_1*/
-            );
-
-            s3 = new AmazonS3Client(credentialsProvider);
-            s3.setRegion(Region.getRegion(Regions.AP_NORTHEAST_1));
-
-            transferUtility = new TransferUtility(s3, getApplicationContext());
-            //.load("https://upload.wikimedia.org/wikipedia/commons/d/d5/Mona_Lisa_(copy,_Hermitage).jpg")
-            String S3_END_POINT = "https://s3-ap-northeast-1.amazonaws.com/"; // public files
-            S3Object s3Object = s3.getObject("pixliapp01","/images/05es.jpg" );
-            String url = S3_END_POINT + s3Object.getBucketName() + "/" + s3Object.getKey();
-
-            System.out.println(" this   "+url);
-
-
-
-
-//        uri = Uri.parse(ss.toString());
-//        System.out.println(""+uri);
-
-
-//            Picasso.with(getApplicationContext())
-//                    .load("https://upload.wikimedia.org/wikipedia/commons/d/d5/Mona_Lisa_(copy,_Hermitage).jpg")
-//                    //.placeholder(R.drawable.image_name_default) //have to put images inside studio for use
-//                    .error(R.drawable.err)
-//                    .resize(50,50)
-//                    .centerInside()
-//                    .into(imageView);
-            return null;
-        }
-    }
-
-
-
-    /*private class DownloadFilesTask extends AsyncTask<url , Integer, Long>
-    {
-        @Override
-        protected Long doInBackground(URL... urls)
-        {
-            int count = urls.length;
-            long totalSize = 0;
-            for (int i = 0; i < count; i++)
-            {
-                Uri uri=null;
+            @Override
+            protected Uri doInBackground(Void... params) {
                 try {
                     java.util.Date expiration = new java.util.Date();
                     long msec = expiration.getTime();
@@ -116,20 +88,23 @@ public class MainActivity extends AppCompatActivity {
                     uri = Uri.parse(ss.toString());
                     System.out.println(""+uri);
                 }catch(Exception e){System.out.println(""+e);}
+
+                return uri;
             }
-            return totalSize;
-        }
 
-        @Override
-        protected void onProgressUpdate(Integer... progress)
-        {
-            setProgressPercent(progress[0]);
-        }
-
-        @Override
-        protected void onPostExecute(Long result) {
-            showDialog("Downloaded " + result + " bytes");
-        }
-    }*/
+            @Override
+            protected void onPostExecute(Uri result) {
+                super.onPostExecute(result);
+                System.out.println("In Post Execute :"+result);
+                Picasso.with(context)
+                        .load(result)
+                        //.placeholder(R.drawable.err)
+                        .error(R.drawable.err)
+                        .resize(500,500)
+                        .centerInside()
+                        .into(imageView);
+            }
+        }.execute();
+    }
 
 }
